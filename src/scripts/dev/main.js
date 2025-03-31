a2f805bd10d81fc73fe0a01ff13ceb0e4ad629d0
Anastasia-Kostyshina <anastasiabones@gmail.com>
2025-03-31 12:56:38 +0700

    changes 31.03.25

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -26,6 +26,29 @@
       });
     });
 
+    // Initializing swiper plugin
+    const swipersSeries = document.querySelectorAll(".js-swiper-series");
+
+    swipersSeries.forEach(function (swpr) {
+      new Swiper(swpr, {
+        updateOnWindowResize: true,
+        slidesPerView: "auto",
+        freeMode: true,
+        spaceBetween: 0,
+        speed: 500,
+        grabCursor: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        navigation: {
+          nextEl: ".swiper-arrow-next",
+          prevEl: ".swiper-arrow-prev",
+          disabledClass: "arrow--disabled"
+        }
+      });
+    });
+
     // Карта 1
     const contactsMap = document.querySelector("#js-contactsMap");
     if (contactsMap) {
